a3e90fa15464b7fb5de1cb60a501d3f6f5ccfc3a
ciaranj <ciaranj@gmail.com>
2010-04-02 17:20:03 +0100

    Got rid of yucky root directory passing

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var sys= require('sys');
-process.__root= __dirname;
 
 require('./lib/ws_server'); // Pull in and start the web socket server
-require('./lib/express_server'); // Pull in and start the express http server
+require('./lib/express_server').startServer(__dirname, 8080); // Pull in and start the express http server
 sys.puts( 'Server Started' )
